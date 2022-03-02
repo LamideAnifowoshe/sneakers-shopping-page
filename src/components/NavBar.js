1fe39cf99a3d64adbcf0ded202ee7efa329705de
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-02 01:10:32 +0100

    Create Navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Logo from "./images/logo.svg";
+import Image1 from "./images/image1.png";
 
 const NavBar = () => {
   return (
@@ -21,24 +24,26 @@ const NavBar = () => {
               <Nav.Link href="#pricing">Women</Nav.Link>
               <Nav.Link href="#pricing">About</Nav.Link>
               <Nav.Link href="#pricing">Contact</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            </Nav>
+            <Nav>
+              <NavDropdown
+                title=<FontAwesomeIcon icon={faCartShopping} />
+                id="collasible-nav-dropdown"
+              >
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
               </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
+
               <Nav.Link eventKey={2} href="#memes">
-                Dank memes
+                <img
+                  src={Image1}
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                  }}
+                />
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
