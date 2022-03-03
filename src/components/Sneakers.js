94bacd2b1433fe34a6c7a86eed29a9e96899ac46
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-03 06:44:54 +0100

    Create Sneakers component

---
+++ src/components/Sneakers.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sneakers = () => {
+  return (
+    <div>
+    <div class="pt-sm-5 col-sm-5">
+                <h6>SNEAKER COMPANY</h6>
+                <h1 class="font-weight-bolder">Fall Limited Edition Sneakers</h1>
+                <p class="pt-4 text-break">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+                
+    </div>
+  )
+}
+
+export default Sneakers
