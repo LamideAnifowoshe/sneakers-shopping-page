e2d5582a19735e0c1197f731aa7ae335b53bf783
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-03 06:48:01 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import NavBar from "./components/NavBar";
+import CarouselItem from "./components/CarouselItem";
+import Container from "react-bootstrap/Container";
+import Sneakers from "./components/Sneakers;
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Container>
+        <Row>
+          <Col sm={6}>
+            <CarouselItem />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
